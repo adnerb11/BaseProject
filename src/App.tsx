4fc3970abdf770b35118e87e0f48d2884aff3bc9
= <=>
2024-07-19 00:19:20 -0500

    Add node modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Landing from './features/landing/pages/Landing'
+
 function App() {
-  return <h1 className="text-3xl font-bold text-blue-700">Telemed app</h1>
+  return <Landing />
 }
 
 export default App
