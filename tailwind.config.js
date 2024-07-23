e55867766c0456d736c1130a2bb5eccf9bfe8c20
Brenda Nahomi Quispe Gonzales <=>
2024-07-22 23:04:59 -0500

    Add structure of login and register

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        authImage: "url('src/assets/sesion/HeroImage.jpeg')",
+      },
+    },
   },
   plugins: [],
-};
+}
